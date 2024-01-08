46f27418b780748fcacab1959f8e157b62d439a2
Hristo Paralinov <110828324+mat43a@users.noreply.github.com>
2024-01-08 19:59:12 +0200

    push

--- exercise_first_steps_in_OOP/pokemon_battle/project/trainer.py
+++ exercise_first_steps_in_OOP/pokemon_battle/project/trainer.py
@@ -7,17 +7,22 @@ class Trainer:
 
     def add_pokemon(self, pokemon: Pokemon):
         if  pokemon.name in [pokemon.name for pokemon in self.pokemons]:
-            return f'This is pokemon is already caught'
+            return f'This pokemon is already caught'
         self.pokemons.append(pokemon)
         return f'Caught {pokemon.pokemon_details()}'
 
     def release_pokemon(self, pokemon_name: str):
-        for pokemon_name in self.pokemons:
-            if pokemon_name == pokemon_name:
-                self.pokemons.remove(pokemon_name)
+        for pokemon_obj in self.pokemons:
+            if pokemon_obj.name == pokemon_name:
+                self.pokemons.remove(pokemon_obj)
                 return f'You have released {pokemon_name}'
         return f'Pokemon is not caught'
 
     def trainer_data(self):
         info = [f'Pokemon Trainer {self.name}', f'Pokemon count {len(self.pokemons)}']
         for pokemon in self.pokemons:
+            info.append(f'- {pokemon.pokemon_details()}')
+        return '\n'.join(info)
+
+
+
